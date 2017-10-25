1c329696b0bb6088b710e5de7d3a772fec781511
Heather DeMarco <hdemarco@msudenver.edu>
2017-10-24 23:22:49 -0600

    Initial commit

---
+++ src/Proj5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Proj5 {
+    public static void main(String[] args) {
+
+        System.out.print("How many values: ");
+        int v = new Scanner(System.in).nextInt();
+
+        System.out.print("Enter the values in order: ");
+        int l = new Scanner(System.in).nextInt();
+
+
+
+    }
+
+
+
+
+}

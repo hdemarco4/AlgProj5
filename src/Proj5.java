4b19fd6655768039bc2d6bcd8f6d3a93b66562dd
hdemarco4 <hdemarco@msudenver.edu>
2017-11-01 22:51:42 -0600

    started tableBuild, wrote pGrid for printing

--- src/Proj5.java
+++ src/Proj5.java
@@ -3,33 +3,76 @@ import java.util.Scanner;
 
 public class Proj5 {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> weights = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> tempw = new ArrayList<Integer>();
+        int d = 0;
 
         System.out.print("number of vertices: ");
         int v = new Scanner(System.in).nextInt();
 
-        System.out.print("weights: ");
-        Scanner w;
+        Object[][][] dGrid = new Object[v][v][2];
 
+        Scanner w = new Scanner(System.in);
+        int[][] weights = new int[v][v];
 
-        for(int i = 0; i < v; i++) {
-            w = new Scanner(System.in);
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                System.out.print("weight: ");
+                int y = w.nextInt();
+                weights[i][j] = y;
 
-            while (w.hasNextInt()) {
-                tempw.add(w.nextInt());
+                if(y == -1) {
+                    dGrid[i][j][0] = '-';
+                    dGrid[i][j][1] = 0;
+                }
+                else {
+                    dGrid[i][j][0] = y;
+                    dGrid[i][j][1] = 0;
+                }
             }
-            weights.add(tempw);
-
         }
+        pGrid(d, dGrid);
 
+        tableBuild(v, 1, dGrid, weights);
+
+            for (int[] y : weights) {
+                for (int z : y) {
+                    System.out.print(z + " ");
+                    }
+                System.out.println();
+            }
 
     }
 
+    public static void tableBuild(int v, int d, Object[][][] dGrid, int[][] weights){
+        
 
 
 
 
+
+
+
+
+        pGrid(d, dGrid);
+        d++;
+        if(d < v){
+            tableBuild(v, d, dGrid, weights);
+        }
+    }
+
+
+    public static void pGrid(int d, Object[][][] dGrid){
+        System.out.println("D " + d);
+        for (Object[][] x : dGrid) {
+            for (Object[] y : x) {
+                for (Object z : y) {
+                    System.out.print(z + " ");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
 
 }

f3ced3f352e88be304095578169666789f65cb85
Heather DeMarco <hdemarco@msudenver.edu>
2017-11-01 20:48:05 -0600

    started

--- src/Proj5.java
+++ src/Proj5.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Proj5 {
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> weights = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> tempw = new ArrayList<Integer>();
 
-        System.out.print("How many values: ");
+        System.out.print("number of vertices: ");
         int v = new Scanner(System.in).nextInt();
 
-        System.out.print("Enter the values in order: ");
-        int l = new Scanner(System.in).nextInt();
+        System.out.print("weights: ");
+        Scanner w;
 
+
+        for(int i = 0; i < v; i++) {
+            w = new Scanner(System.in);
+
+            while (w.hasNextInt()) {
+                tempw.add(w.nextInt());
+            }
+            weights.add(tempw);
+
+        }
 
 
     }
+
+
+
 
 
 
